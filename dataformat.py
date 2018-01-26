804dd7b1d9801ad8db6ed55455cf82f070217639
Rat Boy <unbanmygta@gmail.com>
2018-01-25 20:20:29 -0500

    Initial commit.

--- dataformat.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def formatListData(data):
-    #format data as a JSON string
-    dataString = json.dumps(data)
-    return dataString
--- server.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, request
-import sqlite3
-
-import sys
-import os
