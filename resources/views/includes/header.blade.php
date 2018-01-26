ce12e8fddf2ed13fe753d4c5859c72f275227ea7
Rat Boy <unbanmygta@gmail.com>
2018-01-25 21:15:29 -0500

    Made basic site structure.

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,8 @@
+<div class="navbar">
+    <div class="navbar-inner">
+        <a id="logo" href="/">Project Tentacles</a>
+        <ul class="nav">
+            <li><a href="/">Home</a></li>
+        </ul>
+    </div>
+</div>
