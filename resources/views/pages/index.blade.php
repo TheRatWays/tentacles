2a47374dc82163ec271964e628fcc96e9f8ff937
Rat Boy <unbanmygta@gmail.com>
2018-01-27 17:09:45 +0000

    Added basic layout for viewing videos on index page.

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,5 +3,15 @@
 @section('title', 'Home - Project Tentacles')
 
 @section('content')
-    <p>Testing content...</p>
+    <div class="sectionWrapper">
+        <h2 class="title">Recent Videos</h2>
+
+        <ul class="videos">
+            <li class="video-item">
+                <a class="video-link" href="#">
+                    <!-- <img src=""> -->
+                </a>
+            </li>
+        </ul>
+    </div>
 @endsection
